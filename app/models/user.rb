bb029b5e48b81f0c5c6df884e04f95d42ab9b3e8
twstddev <twstd.dev@gmail.com>
2014-05-04 19:13:53 +0000

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,43 @@
 class User
-  include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	include Mongoid::Document
+	include Mongoid::Timestamps
 
-  ## Database authenticatable
-  field :email,              type: String, default: ""
-  field :encrypted_password, type: String, default: ""
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+		:recoverable, :rememberable, :trackable, :validatable
 
-  ## Recoverable
-  field :reset_password_token,   type: String
-  field :reset_password_sent_at, type: Time
+	## Database authenticatable
+	field :username, type: String
+	field :email,              type: String, default: ""
+	field :encrypted_password, type: String, default: ""
 
-  ## Rememberable
-  field :remember_created_at, type: Time
+	## Recoverable
+	field :reset_password_token,   type: String
+	field :reset_password_sent_at, type: Time
 
-  ## Trackable
-  field :sign_in_count,      type: Integer, default: 0
-  field :current_sign_in_at, type: Time
-  field :last_sign_in_at,    type: Time
-  field :current_sign_in_ip, type: String
-  field :last_sign_in_ip,    type: String
+	## Rememberable
+	field :remember_created_at, type: Time
 
-  ## Confirmable
-  # field :confirmation_token,   type: String
-  # field :confirmed_at,         type: Time
-  # field :confirmation_sent_at, type: Time
-  # field :unconfirmed_email,    type: String # Only if using reconfirmable
+	## Trackable
+	field :sign_in_count,      type: Integer, default: 0
+	field :current_sign_in_at, type: Time
+	field :last_sign_in_at,    type: Time
+	field :current_sign_in_ip, type: String
+	field :last_sign_in_ip,    type: String
 
-  ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
-  # field :locked_at,       type: Time
+	validates :username, presence: true
+	validates :username, uniqueness: true
+	validates :email, presence: true
+
+	## Confirmable
+	# field :confirmation_token,   type: String
+	# field :confirmed_at,         type: Time
+	# field :confirmation_sent_at, type: Time
+	# field :unconfirmed_email,    type: String # Only if using reconfirmable
+
+	## Lockable
+	# field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
+	# field :unlock_token,    type: String # Only if unlock strategy is :email or :both
+	# field :locked_at,       type: Time
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before :each do
+		@user = User.new(
+			username: "admin",
+			email: "admin@admin.com",
+			password: "admin"
+		)
+	end
+
+	it "requires username presence" do
+		@user.username = ""
+		@user.save
+
+		expect( @user ).to have( 1 ).error_on( :username )
+	end
+
+	it "requires username uniqueness" do
+		User.create(
+			username: "admin",
+			email: "another@admin.com",
+			password: "admin123456"
+		)
+
+		@user.save
+
+		expect( @user ).to have( 1 ).error_on( :username )
+	end
+
+	it "requries email presence" do
+		@user.email = ""
+
+		expect( @user ).not_to have( 0 ).error_on( :email )
+	end
 end
