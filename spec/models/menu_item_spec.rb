07a076de7fbc8bbaeeaee93cace2eab82b1a106f
twstddev <twstd.dev@gmail.com>
2014-05-04 18:34:49 +0000

    Add MenuItem spec

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe MenuItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before :each do
+		@menu_item = MenuItem.new(
+			title: "Home",
+			url: "home"
+		)
+	end
+
+	it "requires title" do
+		@menu_item.title = ""
+
+		expect( @menu_item ).to have( 1 ).error_on( :title )
+	end
+
+	it "requires url" do
+		@menu_item.url = ""
+		expect( @menu_item ).to have( 1 ).error_on( :url )
+	end
+
+	it "can have children" do
+		@menu_item.children.create(
+			title: "About",
+			url: "about"
+		)
+
+		expect( @menu_item.children.first.parent_id ).to eq( @menu_item.id )
+	end
 end
