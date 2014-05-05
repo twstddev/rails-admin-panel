93caca18bf7282a8415678325c5999d0c72aaf5f
twstddev <twstd.dev@gmail.com>
2014-05-05 19:04:43 +0000

    Remove previous files

--- app/controllers/json_controller.rb
+++
@@ -1,8 +0,0 @@
-class JsonController < ApplicationController
-	before_filter :check_format
-
-	private
-		def check_format
-			render nothing: true, status: 406, unless params[ :format ] == "json"
-		end
-end
