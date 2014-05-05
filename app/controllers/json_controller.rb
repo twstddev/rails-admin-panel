8363b7a0449970dfda0ea6f8be4b74f946d9e35c
twstddev <twstd.dev@gmail.com>
2014-05-05 18:30:33 +0000

    Add json controller

---
+++ app/controllers/json_controller.rb
@@ -0,0 +1,8 @@
+class JsonController < ApplicationController
+	before_filter :check_format
+
+	private
+		def check_format
+			render nothing: true, status: 406, unless params[ :format ] == "json"
+		end
+end
