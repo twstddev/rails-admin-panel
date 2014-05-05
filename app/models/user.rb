7784caae780cd087e3ee004cbb9ae27fd0fcbffe
twstddev <twstd.dev@gmail.com>
2014-05-05 10:38:33 +0000

    Remove ability to recover password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable,
-		:recoverable, :rememberable, :trackable, :validatable
+		:rememberable, :trackable, :validatable
 
 	## Database authenticatable
 	field :username, type: String
