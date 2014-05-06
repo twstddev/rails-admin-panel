42299e3291758b7e663a9c4d93ec1711315574f1
twstddev <twstd.dev@gmail.com>
2014-05-06 13:40:55 +0000

    Add admin pages index template

--- app/helpers/admin/current_page_helper.rb
+++ app/helpers/admin/current_page_helper.rb
@@ -3,7 +3,7 @@ module Admin::CurrentPageHelper
 	# @brief Checks whether a path is the current.
 	##
 	def is_current_page( path )
-		if current_page? path
+		if path == controller.controller_name
 			return "active"
 		end
 		""
